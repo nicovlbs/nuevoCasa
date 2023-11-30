10dbcb529751498b8dc0de7db9bdd89974471c3a
Nico <Nico@192.168.1.101>
2023-11-30 11:24:49 +0100

    Ejercicios tema 2 subidos

---
+++ Tema1/src/clase1.java
@@ -0,0 +1,27 @@
+
+public class clase1 {
+	
+	public static final Integer MESES_DEL_AÑO = 12;
+	public static final String SALUDO = "hola";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola Mundo");
+		System.out.println(SALUDO + "los meses del año son" + MESES_DEL_AÑO);
+		String mensaje;
+		mensaje = "Hola";
+		mensaje = "Hola" + "que tal";
+		
+		
+		Integer numero1;
+		numero1 = 42;
+		Integer numero2;
+		numero2 = 64;
+		Integer resultado;
+		resultado = numero1+numero2;
+		
+		System.out.println("La suma es " + numero1 + " + " + numero2 + " = " + resultado);
+	}
+
+}
